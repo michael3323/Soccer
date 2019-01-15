f59d8d9d2047124e982300439e196bf996654318
michael3323 <43757874+michael3323@users.noreply.github.com>
2019-01-15 09:19:41 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,19 @@
+
+$("#Germany").click(function(){
+   $("#").slideToggle(); 
+});
+    
+$("#GOD").click(function(){
+    $("#messi").slideToggle();
+
+});
+
+$("#yay").click(function(){
+    $("#strength").fadeToggle();
+
+});
+
+$("#hey").click(function(){
+    $("#hello").slideToggle();
+
+});
